c53745629e2cbbbb06a55c8410e87b953ddd276b
Wuata <lmgom875@gmail.com>
2020-01-04 00:16:41 -0500

    function for update hour, function for change color in rows

--- dayPlannerJquery.js
+++ dayPlannerJquery.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-    var hoursDivNum = 0;
+
+    /* -------------------------- */
+    /* Variables*/
+    /* -------------------------- */
+
     var hourDis = [
         "9AM",
         "10AM",
@@ -11,45 +15,75 @@ $(document).ready(function(){
         "4PM",
         "5PM"
     ]
-    var hourDisPos = 0;
-    
-    var test = test;
+    var timeHours = moment().format('hA');
+    var timeNow = moment(timeHours,'hA');
+    var timeDisplay = moment(hourDis[3],'hA');
+    var timeSame = "";
+    var timeBefore = "";
+    var timeAfter = "";
 
+
+
+    //console.log(hour);
+
+    /* -------------------------- */
+    /* Loading Events */
+    /* -------------------------- */
+
+    //$('#mainHead1').children().last().html("<h6>" + time + "</h6>");
+    hourUpdate();
+    setInterval(hourUpdate, 1000);
     createHoursDiv();
+    colorVerification();
+    
+
+    /* -------------------------- */
+    /* Function*/
+    /* -------------------------- */
 
     function createHoursDiv(){
-        var hourNow = hourDis[hourDisPos];
+        for(i = 0; i < hourDis.length; i++){
         $('#container').append("<div class= 'row mainHours d-flex justify-content-between' id='mainHours'></div>");
-        $('#container').children().last().append('<div class= "col-1 hoursNum" id="hoursNum"><p id="hour">'+ hourNow + '</p>');
+        $('#container').children().last().append('<div class= "col-1 hoursNum" id="hoursNum"><p id="hour">'+ hourDis[i] + '</p>');
         $('#container').children().last().append("<div class= 'col-10 hoursText' id='hoursText'><input type='text' class='inputEvent' id='inputEvent'></div>");
         $('#container').children().last().append("<div class= 'col-1 hoursSaveBtn' id='hoursSaveBtn'><button class='btn btn-primary' id='btnSave'>Save</button></div>");
-        hourDisPos++;
-        hoursDivNum++;
+        }
+    }
+
+    function hourUpdate(){
+        var timeReadable = moment().format('MMMM Do YYYY, h:mm:ss a');
+        $('#mainHead1').children().last().html("<h6>" + timeReadable + "</h6>");
         
-        console.log(hourNow);
-        if(hoursDivNum < 9){
-            createHoursDiv();
+    }
+
+    function colorVerification(){
+        for(i = 0; i < hourDis.length; i++){
+            var timeDisplay = moment(hourDis[i],'hA');
+            timeSame = (moment(timeNow).isSame(timeDisplay));
+            timeAfter = (moment(timeNow).isAfter(timeDisplay));
+            timeBefore = (moment(timeNow).isBefore(timeDisplay));
+            if (timeSame == true) {
+                console.log("presente");
+            }if (timeBefore == true) {
+                console.log("futuro");
+            }if (timeAfter == true) {
+                console.log("pasado")
+            }
+
         }
 
     }
-    
-    
+    console.log(timeNow);
+    //console.log(timeBefore);
+    //console.log("hola afuera");
 
 
-/* -------------------------- */
-/* Variables*/
-/* -------------------------- */
-//console.log("hola afuera");
 
-/* -------------------------- */
-/* Function*/
-/* -------------------------- */
+    /* -------------------------- */
+    /* Events*/
+    /* -------------------------- */
 
-/* -------------------------- */
-/* Events*/
-/* -------------------------- */
 
-//createHoursDiv();
 
 
 });
